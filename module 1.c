9f0ebca1a84fb1e58e084629485e10c1c7bf2700
mihir2052 <vaghelamihirsinh010@gmail.com>
2023-04-15 14:40:43 +0530

    complete
    complete

--- module 1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-main ()
-{
-	printf(" My name is mihir \n");
-	printf(" My birthday is 20 may 2002 \n");
-	printf("My age is 20 years \n");
-	printf(" My address : c,216 Avishkar apartment maktampur \n bhbaruch ");
-}
