1fe5500029fd4426640522bfc438ac81abc67199
mihir2052 <vaghelamihirsinh010@gmail.com>
2023-04-15 14:39:18 +0530

    complete
    complete

---
+++ assignment/module 2/module 2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main()
+{
+	int x,square,cube;
+	printf("enter a value of x: %d \n",x);
+	scanf("%d",&x);
+	square=x*x;
+	cube=x*x*x;
+	printf(" sqaure is =%d \n",square);
+	printf(" cube is = %d \n", cube);
+	return 0;
+	
+	
+}
